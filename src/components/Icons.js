4cb11abe88e669b4fbabb7c8c490a5bdf8c77732
awais-cheema <awaisashraf42000@gmail.com>
2023-10-12 17:27:04 +0500

    T-icon

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -21,7 +21,7 @@ export const TwitterIcon = ({ className, ...rest }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="1em"
-    height="1em"
+    height="1.5em"
     viewBox="0 0 256 256"
     {...rest}
     className={`w-full h-auto ${className}`}
