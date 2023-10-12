0bb7d0816835b49ce3f0af3b18bb0bbee7350504
awais-cheema <awaisashraf42000@gmail.com>
2023-10-12 16:01:17 +0500

    changed main profilePic webp

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Layout from "@/components/Layout";
 import Image from "next/image";
-import profilePic from "../../public/images/profile/PortfolioProfile.png";
+import profilePic from "../../public/images/profile/PortfolioProfile.webp";
 import Animatedtext from "@/components/AnimatedText";
 import Link from "next/link";
 import { LinkArrow } from "@/components/Icons";
