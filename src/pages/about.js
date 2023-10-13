b2d6bf53a1856782e714f81f4ba311edb0dbaf1b
awais-cheema <awaisashraf42000@gmail.com>
2023-10-13 14:34:55 +0500

    changed '+' numbers in about

--- src/pages/about.js
+++ src/pages/about.js
@@ -98,7 +98,7 @@ const about = () => {
             <div className="col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center md:order-3">
               <div className="flex flex-col items-end justify-center xl:!items-center ">
                 <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
-                    <AnimatedNumbers value={50} />+
+                    <AnimatedNumbers value={10} />+
                 </span>
                 <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light xl:text-center md:text-lg sm:text-base xs:text-sm">
                   Satisfied clients
@@ -106,7 +106,7 @@ const about = () => {
               </div>
               <div className="flex flex-col items-end justify-center xl:!items-center ">
                 <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
-                <AnimatedNumbers value={40} />+
+                <AnimatedNumbers value={20} />+
                 </span>
                 <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light xl:text-center md:text-lg sm:text-base xs:text-sm">
                   Projects Completed
@@ -114,10 +114,10 @@ const about = () => {
               </div>
               <div className="flex flex-col items-end justify-center xl:!items-center ">
                 <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
-                <AnimatedNumbers value={4} />+
+                <AnimatedNumbers value={12} />+
                 </span>
                 <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light xl:text-center md:text-lg sm:text-base xs:text-sm">
-                  Years of Experience
+                  Months of Experience
                 </h2>
               </div>
             </div>
